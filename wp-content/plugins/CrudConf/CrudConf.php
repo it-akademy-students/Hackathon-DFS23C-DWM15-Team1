e5f2e94178885b588a938fcf09180aa0c70438a4
Yashoo69 <t.vidal@it-students.fr>
2021-10-12 19:47:52 +0200

    plug-in Crud Conférence

---
+++ wp-content/plugins/CrudConf/CrudConf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Conférence_Update
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Update Conférences
+Description: Un plugin pour pouvoir modifier et/ou créer une conférence
+Author: Thomas Vidal
+Version: 1.0.0
+Author URI:
+*/
